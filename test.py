052e4af799babf4e0c0f9eb396ffe7776ac084e0
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2022-05-30 20:53:06 +0100

    Added test framework

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestIdrisTools(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(1+1, 2, "Should be 2")
+
+if __name__ == '__main__':
+    unittest.main()
