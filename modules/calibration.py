35b3ac1e4edc5d8cd4d20e625e6458c7733ae680
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2022-06-04 13:20:38 +0100

    Added documentation to calibration.py

--- modules/calibration.py
+++ modules/calibration.py
@@ -11,17 +11,68 @@ __email__ = "j.d.hayward@surrey.ac.uk"
 __status__ = "Indev"
 
 class Calibration:
+    """ Calibrates one set of measurements against another 
+
+    Attributes:
+        - x (dict): Measurements to be calibrated against. Keys include:
+            - 'Measurements' (dict): Contains two keys:
+                - 'Values' (list): Measurements 
+                - 'Timestamps' (list): Times measurements made
+            - 'Name' (str): Name of device
+        - y (dict): Measurements to be calibrated. Keys include:
+            - 'Measurements' (dict): Contains two keys:
+                - 'Values' (list): Measurements 
+                - 'Timestamps' (list): Times measurements made
+            - 'Name' (str): Name of device
+            - 'Secondary Measurements' (dict): Contains keys representing
+            the different secondary variables. Can be empty:
+                - *variable* (list): Contains list of measurements
+
+    Methods:
+        - ols_linear: Performs OLS linear regression 
+
+        - maximum_a_posteriori: Performs MAP regression 
+
+        - bayesian: Performs bayesian linear regression (uni or multi)
+
+        - robust_bayesian: Performs robust bayesian linear regression (uni or
+        multi)
+
+        - multivariate: Performs multivariate OLS linear regression
+
+        - rolling: Performs rolling OLS 
+
+        - appended: Performs appended OLS
     """
-    """
-    def __init__(self, x_data, y_data, y_mv_data=[]):
+    def __init__(self, x_data, y_data):
         """ Initialises the calibration class 
 
         This class is used to compare one set of measurements against another.
         It also has the capability to perform multivariate calibrations when
         secondary variables are provided.
 
+        Keyword Arguments:
+            - x_data (dict): Dict of the data that y is to be calibrated
+            against. Keys includes:
+                - 'Measurements' (dict): Contains two keys:
+                    - 'Values' (list): Measurements 
+                    - 'Timestamps' (list): Times measurements made
+                - 'Name' (str): Name of device
+    
+            - y_data (dict): Dict of the data that is to be calibrated
+            Keys includes:
+                - 'Measurements' (dict): Contains two keys:
+                    - 'Values' (list): Measurements 
+                    - 'Timestamps' (list): Times measurements made
+                - 'Name' (str): Name of device
+                - 'Secondary Measurements' (dict): Contains keys representing
+                the different secondary variables. Can be empty:
+                    - *variable* (list): Contains list of measurements
+                     
+
         """
-        pass
+        self.x = x_data 
+        self.y = y_data 
 
     def ols_linear(self):
         """ Performs OLS linear regression comparing y against x
