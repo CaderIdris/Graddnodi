5558a8e45c418bf75a5dcab328bc98214bed2b9c
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2022-10-30 21:28:44 +0000

    best_performing function now returns different counts based on whether 'key' or 'col' is passed. 'key' counts by dictionary key, 'col' counts by dataframe column
    Removed warning from _check_valid and put it in individual functions, makes more sense

--- modules/summary.py
+++ modules/summary.py
@@ -1,6 +1,14 @@
+from collections import defaultdict
 import warnings
 
 import pandas as pd
+
+class ArgumentWarning(Warning):
+    def __init__(self, message):
+        self.message = message 
+
+    def __str__(self):
+        return repr(self.message)
 
 class Summary:
     def __init__(self, dataframes):
@@ -31,34 +39,47 @@ class Summary:
     def min(self):
         return self.group.min()
 
-    def _check_valid(self, function_name):
+    def _check_valid(self):
         if isinstance(self._dataframes, dict):
             return True
         else:
-            warnings.warn(
-                    f"{function_name} expects dataframes to be " 
-                    f"represented as a dict"
-                    )
             return False
 
     def diff_from_mean(self):
-        if self._check_valid("diff_from_mean"):
+        if self._check_valid():
             diff_dict = dict()
             mean_df = self.mean()
             for key, df in self._dataframes.items():
                 diff_dict[key] = df - mean_df
             return diff_dict
         else:
+            warnings.warn("diff_from_mean method requires the _dataframes "
+                          "instance to be a dict. Please pass a dict to the "
+                          "dataframesm arg when initialising the Summary "
+                          "class to use this method", ArgumentWarning)
             return None
 
-    def best_performing(self):
-        if self._check_valid("best_performing"):
+    def best_performing(self, summate='key'):
+        if self._check_valid():
             count_dict = dict()
             min_df = self.min()
-            for key, df in self._dataframes.items():
-                count_dict[key] = (df == min_df).values.sum()
-            return count_dict
+            if summate == 'key':
+                for key, df in self._dataframes.items():
+                    count_dict[key] = (df == min_df).sum(axis=1).to_dict()
+                return count_dict
+            elif summate == 'col':
+                count_series = pd.Series(dtype=int)
+                for key, df in self._dataframes.items():
+                    count_series.add((df == min_df).sum(), fill_value=0)
+                return count_series.to_dict()
+            else:
+                warnings.warn("summate keyword argument should be key or col",
+                              ArgumentWarning)
         else:
+            warnings.warn("best_performing method requires the _dataframes "
+                          "instance to be a dict. Please pass a dict to the "
+                          "dataframesm arg when initialising the Summary "
+                          "class to use this method", ArgumentWarning)
             return None
 
 
